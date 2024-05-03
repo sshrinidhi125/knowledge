7ac15b052c18db58ee0049bc28cd2c07f582a339
sshrinidhi125 <sshrinidhi125@gmail.com>
2024-05-03 11:26:45 +0530

    Create prime_number.py

---
+++ python/prime_number.py
@@ -0,0 +1,16 @@
+import math
+num=int(input("enter number :"))
+is_prime = True
+factor = 1
+#for i in range(2,num-1):
+for i in range(2, int(math.sqrt(num))+1):
+    if num%i == 0:
+        is_prime = False
+        factor = i
+        break
+
+if is_prime:
+    print(num, " is a prime number")
+else:
+    print(num, " is not a prime number and its factor is " , factor, num//factor)
+
